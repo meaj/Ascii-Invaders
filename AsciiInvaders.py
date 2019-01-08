c92c6eb7768561beba47b9c46747f8c36ab4d4fb
Meaj <kevin.omd@gmail.com>
2019-01-08 03:03:56 -0600

    Movement Update
    * Console now fits size of game grid
    * Enemies now move side to side and down (with some some values shifting)
    * Added basis for collision checking and life deduction/scoring

--- AsciiInvaders.py
+++ AsciiInvaders.py
@@ -58,7 +58,7 @@ def get_keypress(player_entity, list_of_projectiles):
 # Resets player position and lowers lives
 def kill_player(int_lives, player_entity):
     int_lives -= 1
-    player_entity = [14, 24]
+    player_entity = [14, 24, 0, 0, -1]
     return int_lives, player_entity
 
 
@@ -68,6 +68,52 @@ def clear():
         _ = os.system('cls')
     else:
         _ = os.system("clear")
+
+
+# Checks for collisions between entities
+def has_collision(entity_one, entity_two):
+    if entity_one == entity_two:
+        return True
+    return False
+
+
+# Moves the projectiles
+def move_projectiles(list_of_projectiles):
+    for projectile_entity in list_of_projectiles:
+        if -1 < projectile_entity[0] < 15:
+            projectile_entity[0] += projectile_entity[2]
+        else:
+            projectile_entity = [-1, -1, 0]
+    return list_of_projectiles
+
+
+# Moves the enemies
+def move_enemies(list_of_enemies, int_direction, int_lives):
+    list_of_future_positions = list_of_enemies
+    bool_shift_down = False
+
+    # Move all enemies in the current direction
+    for temp_entity in list_of_future_positions:
+        # If any entity is past either boundary, cancel directional movement and shift down
+        if temp_entity[1] + int_direction > 49 or temp_entity[1] + int_direction < -1:
+            int_direction *= -1
+            bool_shift_down = True
+            break
+        temp_entity[1] += int_direction
+
+    if bool_shift_down:
+        for enemy_entity in list_of_enemies:
+            enemy_entity[0] += 1
+            # If any entity shifts down off the screen, deduct a life
+            if enemy_entity[0] == 14:
+                int_lives -=1
+                enemy_entity = [-1, -1, 0, 0, 1]
+    else:
+        list_of_enemies = list_of_future_positions
+
+    print(list_of_enemies)
+
+    return list_of_enemies, int_direction, int_lives
 
 
 # Updates the positions of player and enemies on the game board
@@ -81,7 +127,8 @@ def update_board(list_of_enemies, player_entity, list_of_projectiles, game_grid)
     # update game grid with all enemy positions
     for enemy_entity in list_of_enemies:
         # print("Printing enemy at position: " + str(i) + ":" + str(j))
-        game_grid[enemy_entity[0]][enemy_entity[1]] = 'v'
+        if enemy_entity[0] != -1:
+            game_grid[enemy_entity[0]][enemy_entity[1]] = 'v'
 
     # update game grid with all projectile positions
     for projectile_entity in list_of_projectiles:
@@ -109,16 +156,20 @@ def print_board(int_lives, int_score, game_grid):
 
 
 def main():
+    # Set console to fit game grid
+    os.system('mode con: cols=52 lines=16')
+
     # Main game variables
     int_score = 0
     int_lives = 3
+    int_direction = 1
 
     # Player entity, has position at default location, with fire state of 0, fire order of 0, and heading of -1
     player_entity = [14, 24, 0, 0, -1]
 
     # Array of enemy entities, which have locations which are set to default positions
-    # and fire states of 0 as well as fire order assignment and heading of 1
-    list_of_enemies = [[-1, -1, 0, 1] for i in range(60)]
+    # and fire states of 0 as well as fire order assignment, and heading of 1
+    list_of_enemies = [[-1, -1, 0, 0, 1] for i in range(30)]
     list_of_enemies = set_enemies_to_default(list_of_enemies)
 
     # Grid which will contain the game entities
@@ -129,13 +180,27 @@ def main():
 
     # Game Loop
     print(list_of_enemies)
-    while int_lives != 0 and int_score < 600:
+    while int_lives != 0 and int_score < 300:
 
         # Input Checking
         get_keypress(player_entity, list_of_projectiles)
-        # Collision Checking
 
-        # Position Checking
+        # Projectile movement
+        list_of_projectiles = move_projectiles(list_of_projectiles)
+
+        # Check for enemy collision with player projectile
+        for enemy_entity in list_of_enemies:
+            if has_collision(enemy_entity, list_of_projectiles[0]):
+                int_score += 10
+                enemy_entity[0] = [-1, -1, 0, 0, 1]
+
+        # Checks for projectile collision with player
+        for projectile_entity in list_of_projectiles:
+            if has_collision(player_entity, projectile_entity):
+                int_lives, player_entity = kill_player(int_lives, player_entity)
+
+        # Enemy Movement
+        list_of_enemies, int_direction, int_lives = move_enemies(list_of_enemies, int_direction, int_lives)
 
         # Updates the Game Grid after position checking
         game_grid = update_board(list_of_enemies, player_entity, list_of_projectiles, game_grid)
@@ -143,8 +208,8 @@ def main():
         # Prints Game Grid after updating
         print_board(int_lives, int_score, game_grid)
 
-        # Sleeps for 1/30th of a second
-        time.sleep(.032)
+        # Sleeps for 1/15th of a second
+        time.sleep(.064)
 
         # Used for testing starting frame:
         # int_lives = 0
